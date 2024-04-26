ca9efa21f88a7878060fb9b6e9f3d330f85384fc
jerem-marti <jeremy_martin@bluewin.ch>
2024-04-26 16:14:43 +0200

    error reading file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import dsv from '@rollup/plugin-dsv'
 
 export default defineConfig({
     plugins: [
         dsv(),
-],
+        nodePolyfills(),
+    ],
 })
